0ca2671ba1cdc4363e251a7e491891d161af2e16
Bear <69819027+bearbearsbarebear@users.noreply.github.com>
2021-09-08 15:09:37 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
 classes = ["beast", "plant", "bug", "mech", "dusk", "aquatic", "bird", "reptile", "dawn"]
-stats = ["health", "speed", "skill", "morale"]
+stats = ["hp", "speed", "skill", "morale"]
 
 axie_brief_list = {
   "operationName": "GetAxieBriefList",
@@ -15,32 +15,35 @@ axie_brief_list = {
   "query": "query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"
 }
 
-def get_query(query_name, query_list):
-  if (query_name == "axie_brief_list"):
-    criteria = {}
+def get_axie_brief_list(query_list):
+  criteria = {}
+  has_price = 0
 
-    for i in query_list:
-      for j in range(len(i)):
-        if (i[j] in stats):
-          if (i[j] == "health"):
-            criteria['hp'] = [i[j + 1], i[j + 2]]
-          else:
-            criteria[i[j]] = [i[j + 1], i[j + 2]]
-          continue
+  for i in range(len(query_list)):
+    for j in range(len(query_list[i])):
+      # Class
+      if (query_list[i][j] == 'class'):
+        criteria['classes'] = [query_list[i][j+1][0].upper() + query_list[i][j+1][1:]]
+        #print("Class: {0}".format(query_list[i][j+1]))
+      # Stats
+      elif (query_list[i][j] in stats):
+        criteria[query_list[i][j]] = [query_list[i][j+1], query_list[i][j+2]]
+        #print("{0}: {1}-{2}".format(query_list[i][j], query_list[i][j+1], query_list[i][j+2]))
+      # Parts list
+      elif (query_list[i][j] == 'part'):
+        criteria['parts'] = query_list[i][j+1]
+        #print(query_list[i][j+1])
+      elif (query_list[i][j] == 'price'):
+        has_price = 1
 
-        if (i[j] == "class"):
-          criteria['classes'] = [i[j+1][0].upper() + i[j+1][1:]]
-          continue
+  if (has_price == 0):
+    print("Alert: no min price at the query: {0}".format(query_list))
 
-        if (i[j] == "part"):
-          criteria['parts'] = i[j + 1]
-          continue
+  query = axie_brief_list
+  query["variables"]["criteria"] = criteria
+  r = requests.post('https://graphql-gateway.axieinfinity.com/graphql', json = query)
 
-    query = axie_brief_list
-    query["variables"]["criteria"] = criteria
-    r = requests.post('https://axieinfinity.com/graphql-server-v2/graphql', json = query)
-
-    return r
+  return r
 
 def retrieve_axies_list(response):
   axies_list = []
@@ -66,9 +69,6 @@ def get_price_min(query):
     for j in query[i]:
       if (j == "price"):
         return float(query[i][1])
-
-def axies_total(response):
-  return response.json()['data']['axies']['total']
 
 def remove_query(query):
   while (True):
